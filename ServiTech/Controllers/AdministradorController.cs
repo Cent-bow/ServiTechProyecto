432973785be08b5634aa6a540eccf01fc08285de
Centhia Bowman <Centhiabowman@outlook.com>
2021-05-18 18:38:38 -0400

    Agregar archivos de proyecto.

---
+++ ServiTech/Controllers/AdministradorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiTech.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministradorController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+        public IActionResult CrearRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearRole(string Nombre)
+        {
+            await _roleManager.CreateAsync(new IdentityRole(Nombre));
+            return View();
+        }
+
+
+
+    }
+}
