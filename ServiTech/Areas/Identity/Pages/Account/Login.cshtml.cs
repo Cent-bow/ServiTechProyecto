03112a3a69ffc907f5cba2c71b2b0d7b968edc7c
Centhia Bowman <Centhiabowman@outlook.com>
2021-05-20 19:01:02 -0400

    a

--- ServiTech/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ServiTech/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace ServiTech.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "¿Desea recordar su cuenta?")]
             public bool RememberMe { get; set; }
         }
 
